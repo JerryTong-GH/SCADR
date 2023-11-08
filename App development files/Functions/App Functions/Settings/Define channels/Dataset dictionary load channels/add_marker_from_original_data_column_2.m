4998017ecf62abaa8c41927442182abbed7f56ab
madhirfromwarwick <mahir.taher@warwick.ac.uk>
2023-11-08 14:42:09 +0000

    update select all markers button (uncompiled and untested)

--- App development files/Functions/App Functions/Settings/Define channels/Dataset dictionary load channels/add_marker_from_original_data_column_2.m
+++ App development files/Functions/App Functions/Settings/Define channels/Dataset dictionary load channels/add_marker_from_original_data_column_2.m
@@ -1,25 +1,22 @@
-function [old_column_names_list,new_column_names_list] = add_marker_from_original_data_column_2(old_column_names_list,new_column_names_list,old_column_names,marker_list)
+function [old_column_names_list,new_column_names_list] = add_marker_from_original_data_column_2( ...
+    old_column_names_list,new_column_names_list,old_column_names,marker_list, ...
+    options)
 %change_add_column_names Create popup ui to select old column name and write new column name
 %   Detailed explanation goes here
+%% Arguments
+arguments(Input)
+    old_column_names_list
+    new_column_names_list
+    old_column_names
+    marker_list
+end
 
-%create popup figure
-fig = uifigure("Name","Add marker channel from original channel name", ...
-    "CloseRequestFcn",@(~,~) add_marker_from_original_data_column_cancel());
-fig.Position(3:4) = [430,180];
+arguments(Input)
+    options.select_all = 0;
+end
 
-%old column drop down text
-old_column_label = uilabel(fig, ...
-    'Position',[10,160,300,20], ...
-    'Text',"Select old column name/s", ...
-    'WordWrap','on','FontSize',14);
-
-%pick out column names from first dataset loaded in
-% dataset_list = total_data_sets.keys;
-% variants_pack = total_data_sets(dataset_list(1));
-% cell_line_data = variants_pack.values{1};
-% old_column_names = cell_line_data.Properties.VariableNames; %old column names that may/may not have been taken
-
-
+%% Function starts
+%% Get available markers to pick from
 overlap = intersect(string(old_column_names),string(marker_list)); %old column names that have been renamed
 
 if numel(overlap)>0
@@ -28,26 +25,44 @@ if numel(overlap)>0
     end
 end
 
-%create listbox of old columns to choose from
-dd = uilistbox(fig,...
-    'Position',[10 10 300 135],...
-    'Items',old_column_names, ...
-    'Multiselect','on');
+%% Select all markers or select manually
+if options.select_all
+    old_column_names_list = [old_column_names_list, old_column_names];
+    new_column_names_list = [new_column_names_list, old_column_names];
 
-%'Add' button
-add_button = uibutton(fig, ...
-    'Position',[320 100 100 22],...
-    "Text","Add", ...
-    'ButtonPushedFcn',@(~,~) add_old_name_to_marker_list());
+else
+    %% Create figure to interactively pick
+    %create popup figure
+    fig = uifigure("Name","Add marker channel from original channel name", ...
+        "CloseRequestFcn",@(~,~) add_marker_from_original_data_column_cancel());
+    fig.Position(3:4) = [430,180];
 
-%'Cancel' button
-cancel_button = uibutton(fig, ...
-    'Position',[320 40 100 22],...
-    "Text","Cancel", ...
-    'ButtonPushedFcn',@(~,~) add_marker_from_original_data_column_cancel());
+    %old column drop down text
+    old_column_label = uilabel(fig, ...
+        'Position',[10,160,300,20], ...
+        'Text',"Select old column name/s", ...
+        'WordWrap','on','FontSize',14);
 
+    %create listbox of old columns to choose from
+    dd = uilistbox(fig,...
+        'Position',[10 10 300 135],...
+        'Items',old_column_names, ...
+        'Multiselect','on');
 
-uiwait(fig);
+    %'Add' button
+    add_button = uibutton(fig, ...
+        'Position',[320 100 100 22],...
+        "Text","Add", ...
+        'ButtonPushedFcn',@(~,~) add_old_name_to_marker_list());
+
+    %'Cancel' button
+    cancel_button = uibutton(fig, ...
+        'Position',[320 40 100 22],...
+        "Text","Cancel", ...
+        'ButtonPushedFcn',@(~,~) add_marker_from_original_data_column_cancel());
+
+    uiwait(fig);
+end
 
     function add_marker_from_original_data_column_cancel()
         delete(fig)
